2d75a4448d18cf275888ba1fda251f47251a080b
yerkebulan19971212 <yerkebulan19971212@gmail.com>
2021-04-04 20:22:12 +0600

    git commit

--- Organization/models.py
+++ Organization/models.py
@@ -4,133 +4,129 @@ from django.contrib.auth.validators import UnicodeUsernameValidator
 
 
 class Bank(models.Model):
-    bik = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=255, default='')
-    city = models.CharField(max_length=255, default='')
+    bik = models.IntegerField(primary_key=True, verbose_name='БИК')
+    name = models.CharField(max_length=255, default='', verbose_name='Атауы')
+    city = models.CharField(max_length=255, default='', verbose_name='Қала')
     correspondent_account = models.IntegerField()
-
-    class Meta:
-        verbose_name = "Банк"
-        verbose_name_plural = "Банк"
 
     def __str__(self):
         return self.name
 
     class Meta:
         verbose_name = "Банк"
-        verbose_name_plural = "Банки"
+        verbose_name_plural = "Банктар"
 
 
 class Valuta(models.Model):
-    code = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=255, default='')
+    code = models.IntegerField(primary_key=True, verbose_name='Код')
+    name = models.CharField(max_length=255, default='', verbose_name='Атауы')
 
     def __str__(self):
         return self.name
 
     class Meta:
         verbose_name = "Валюта"
-        verbose_name_plural = "Валюты"
+        verbose_name_plural = "Валютлар"
 
 
 class Client(AbstractUser):
-    code = models.IntegerField(primary_key=True)
+    code = models.IntegerField(primary_key=True, verbose_name='Код')
     username_validator = UnicodeUsernameValidator()
     username = models.CharField(
         max_length=150,
         unique=True,
         validators=[username_validator],
-        null=True
+        null=True, verbose_name='клиенттің логині'
     )
-    password = models.CharField(max_length=128, default='')
+    password = models.CharField(max_length=128, default='', verbose_name='пароль')
     # fio = models.CharField(max_length=500, default='')
-    passport = models.CharField(max_length=50, default='', unique=True)
+    passport = models.CharField(max_length=50, default='', unique=True, verbose_name='паспорт')
 
     def __str__(self):
         return "".join([self.first_name, " ", self.last_name])
 
     class Meta:
         verbose_name = "Клиент"
-        verbose_name_plural = "Клиенты"
+        verbose_name_plural = "Клиенттер"
 
 
 class ClientValutaAccount(models.Model):
-    code_valuta = models.ForeignKey(Valuta, on_delete=models.CASCADE)
-    code_client = models.ForeignKey(Client, on_delete=models.CASCADE)
-    bik = models.ForeignKey(Bank, on_delete=models.CASCADE)
-    account_number = models.CharField(max_length=50, default='')
+    code_valuta = models.ForeignKey(Valuta, on_delete=models.CASCADE, verbose_name='валютаның коды')
+    code_client = models.ForeignKey(Client, on_delete=models.CASCADE, verbose_name='клиентің коды')
+    bik = models.ForeignKey(Bank, on_delete=models.CASCADE, verbose_name='')
+    account_number = models.CharField(max_length=50, default='', verbose_name='аккаунт номері')
 
     def __str__(self):
         return "".join([self.code_valuta.name, " ", self.code_client.first_name])
 
     class Meta:
-        verbose_name = "Валютные счета Клиента"
-        verbose_name_plural = "Валютные счета Клиента"
+        verbose_name = "Клиенттің валютадағы шоттары"
+        verbose_name_plural = "Клиенттердің валютадағы шоттары"
 
 
 class Organization(models.Model):
-    code = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=500, default='')
-    inn = models.CharField(max_length=50, default='')
-    kpp = models.CharField(max_length=50, default='')
-    address = models.CharField(max_length=50, default='')
-    phone = models.CharField(max_length=11, default='')
-    facs = models.CharField(max_length=50, default='')
-    director = models.CharField(max_length=50, default='')
-    buhgalter = models.CharField(max_length=50, default='')
-    okpo = models.CharField(max_length=50, default='')
-    pc = models.CharField(max_length=50, default='')
-    bik = models.ForeignKey(Bank, on_delete=models.CASCADE)
+    code = models.IntegerField(primary_key=True, verbose_name='Код')
+    name = models.CharField(max_length=500, default='', verbose_name='Атауы')
+    inn = models.CharField(max_length=50, default='', verbose_name='ИНН')
+    kpp = models.CharField(max_length=50, default='', verbose_name='КПП')
+    address = models.CharField(max_length=50, default='', verbose_name='Мекен-жайы')
+    phone = models.CharField(max_length=11, default='', verbose_name='Телефон номері')
+    facs = models.CharField(max_length=50, default='', verbose_name='Факс')
+    director = models.CharField(max_length=50, default='', verbose_name='Директор')
+    buhgalter = models.CharField(max_length=50, default='', verbose_name='Бугалтер')
+    okpo = models.CharField(max_length=50, default='', verbose_name='ОКПО')
+    pc = models.CharField(max_length=50, default='', verbose_name='ПК')
+    bik = models.ForeignKey(Bank, on_delete=models.CASCADE, verbose_name='БИК')
 
     def __str__(self):
         return str(self.name)
 
     class Meta:
-        verbose_name = "Организация"
-        verbose_name_plural = "Организация"
+        verbose_name = "Ұйым"
+        verbose_name_plural = "Ұйымдар"
 
 
 class OrganizationValutaAccount(models.Model):
-    code_valuta = models.ForeignKey(Valuta, on_delete=models.CASCADE)
-    code_organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
-    account_number = models.CharField(max_length=50, default='')
+    code_valuta = models.ForeignKey(Valuta, on_delete=models.CASCADE, verbose_name='Валюта коды')
+    code_organization = models.ForeignKey(Organization, on_delete=models.CASCADE, verbose_name='Ұйым коды')
+    account_number = models.CharField(max_length=50, default='', verbose_name='акканут номері')
 
     def __str__(self):
         return "".join([self.code_valuta.name, " ", self.code_organization.name])
 
     class Meta:
-        verbose_name = "Валютные счета организации"
-        verbose_name_plural = "Валютные счета организации"
+        verbose_name = "Ұйымның валюталық шоттары"
+        verbose_name_plural = "Ұйымның валюталық шоттары"
 
 
 class Contract(models.Model):
-    date = models.DateField()
-    code_organization = models.ForeignKey(Organization, on_delete=models.CASCADE, null=True)
-    code_client = models.ForeignKey(Client, on_delete=models.CASCADE, null=True)
+    date = models.DateField( verbose_name='Дата')
+    code_organization = models.ForeignKey(Organization, on_delete=models.CASCADE, null=True, verbose_name='Ұйым коды')
+    code_client = models.ForeignKey(Client, on_delete=models.CASCADE, null=True, verbose_name='Клиент коды')
 
     def __str__(self):
         return "".join(["Организация: ", self.code_organization.name, ", Клиент: ", self.code_client.first_name, " ",
                         self.code_client.last_name])
 
     class Meta:
-        verbose_name = "Договоры"
-        verbose_name_plural = "Договоры"
+        verbose_name = "Келісім шарт"
+        verbose_name_plural = "Келісім шарттар"
 
 
 class OperationCategory(models.Model):
-    name = models.CharField(max_length=50, default='')
+    name = models.CharField(max_length=50, default='', verbose_name='Атауы')
 
     def __str__(self):
         return self.name
 
 
 class Operation(models.Model):
-    code = models.IntegerField(primary_key=True)
-    category = models.ForeignKey(OperationCategory, on_delete=models.CASCADE)
-    date = models.DateField()
-    contract = models.ForeignKey(Contract, on_delete=models.CASCADE)
-    code_valuta = models.ForeignKey(Valuta, on_delete=models.CASCADE)
-    sum = models.IntegerField()
+    code = models.IntegerField(primary_key=True, verbose_name='Коды')
+    category = models.ForeignKey(OperationCategory, on_delete=models.CASCADE, verbose_name='Категориясы')
+    date = models.DateField(verbose_name='Дата')
+    contract = models.ForeignKey(Contract, on_delete=models.CASCADE, verbose_name='Контракт')
+    code_valuta = models.ForeignKey(Valuta, on_delete=models.CASCADE, verbose_name='Валюта коды')
+    sum = models.IntegerField(verbose_name='Соммасы')
 
     def __str__(self):
         return "".join([self.category.name, "-> Организация: ", self.contract.code_organization.name, ", Клиент: ",
@@ -138,46 +134,46 @@ class Operation(models.Model):
                         self.contract.code_client.last_name, " "])
 
     class Meta:
-        verbose_name = "Операции"
-        verbose_name_plural = "Операции"
+        verbose_name = "Операция"
+        verbose_name_plural = "Операциялар"
 
 
 class Sotrudnik(models.Model):
-    code = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=50)
+    code = models.IntegerField(primary_key=True, verbose_name='Код')
+    name = models.CharField(max_length=50, verbose_name='Атауы')
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name = "Сотрудники"
-        verbose_name_plural = "Сотрудники"
+        verbose_name = "Қызметкер"
+        verbose_name_plural = "Қызметкерлер"
 
 
 class CursValuta(models.Model):
-    date = models.DateField()
-    code_valuta = models.ForeignKey(Valuta, on_delete=models.CASCADE)
-    curs = models.FloatField()
-    cursProd = models.FloatField()
-    cursPokup = models.FloatField()
+    date = models.DateField(verbose_name='Дата')
+    code_valuta = models.ForeignKey(Valuta, on_delete=models.CASCADE, verbose_name='Валюта коды')
+    curs = models.FloatField(verbose_name='Курс')
+    cursProd = models.FloatField(verbose_name='Сатылым Курсы')
+    cursPokup = models.FloatField(verbose_name='Сатып алу курсы')
 
     def __str__(self):
         return "".join([self.code_valuta.name, " -> ", self.date.__str__()])
 
     class Meta:
-        verbose_name = "Курсы валют"
-        verbose_name_plural = "Курсы валют"
+        verbose_name = "Валюта бағамы"
+        verbose_name_plural = "Валюта бағамдары"
 
 
 class Session(models.Model):
-    date = models.DateField()
-    sotrudnik = models.ForeignKey(Sotrudnik, on_delete=models.CASCADE)
-    cursValuta = models.ForeignKey(CursValuta, on_delete=models.CASCADE)
-    operation = models.ForeignKey(Operation, on_delete=models.CASCADE, null=True, blank=True)
+    date = models.DateField(verbose_name='Дата')
+    sotrudnik = models.ForeignKey(Sotrudnik, on_delete=models.CASCADE, verbose_name='Қызметкер')
+    cursValuta = models.ForeignKey(CursValuta, on_delete=models.CASCADE, verbose_name='Валюта курсы')
+    operation = models.ForeignKey(Operation, on_delete=models.CASCADE, null=True, blank=True, verbose_name='Операция')
 
     def __str__(self):
         return "".join(["Сотрудник: ", self.sotrudnik.name, ", ", self.operation.__str__()])
 
     class Meta:
-        verbose_name = "Сессии"
-        verbose_name_plural = "Сессии"
+        verbose_name = "Сессия"
+        verbose_name_plural = "Сессиялар"
